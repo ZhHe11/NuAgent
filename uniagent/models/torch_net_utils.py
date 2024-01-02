5f4cc5f34ac2103c71e695d8869d8828777bae3f
Ming Zhou <zhouming@pjlab.org.cn>
2024-01-02 17:39:58 +0800

    add minigpt4

--- uniagent/models/torch_net_utils.py
+++ uniagent/models/torch_net_utils.py
@@ -1,3 +1,6 @@
+import math
+import torch
+
 from torch import nn
 
 
@@ -15,13 +18,25 @@ class View(nn.Module):
 
     def forward(self, x):
         if self.batched:
-            print(
-                "xxxxxxxxxxxxx",
-                x.shape,
-                x.size(0),
-                self.shape,
-                x.reshape(-1, *self.shape).shape,
-            )
             return x.view(-1, *self.shape)
         else:
             return x.view(*self.shape)
+
+
+class NewGELU(nn.Module):
+    """
+    Implementation of the GELU activation function currently in Google BERT repo (identical to OpenAI GPT).
+    Reference: Gaussian Error Linear Units (GELU) paper: https://arxiv.org/abs/1606.08415
+    """
+
+    def forward(self, x):
+        return (
+            0.5
+            * x
+            * (
+                1.0
+                + torch.tanh(
+                    math.sqrt(2.0 / math.pi) * (x + 0.044715 * torch.pow(x, 3.0))
+                )
+            )
+        )
