b68bbfb9d627ab95c688d1b066df367da7d1bb7d
ming <kornbergfresnel@outlook.com>
2024-04-16 16:10:09 +0800

    init dockers

--- examples/hilp_gcrl.py
+++ examples/hilp_gcrl.py
@@ -3,3 +3,7 @@ from application.hilp.cli import main, get_command_parser
 if __name__ == "__main__":
     args = get_command_parser()
     main(args)
+
+
+# run as:
+# docker run -d --gpus device=0 -v /home/zhouming/projects/universal-agent/:/hilp -v /home/zhouming/.d4rl:/root/.d4rl --name gcrl hilp /bin/bash -c "PYTHONPATH=. python example/hilp_gcrl.py --env_name antmaze-large-diverse-v2 --use_rnd --device cuda"
