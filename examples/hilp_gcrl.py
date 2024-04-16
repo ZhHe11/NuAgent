0412fd592fcaba3c383fbeca4f2493cc4890bba0
ming <kornbergfresnel@outlook.com>
2024-04-16 17:04:59 +0800

    update requirements

--- examples/hilp_gcrl.py
+++ examples/hilp_gcrl.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
 
 
 # run as:
-# docker run -d --gpus device=0 -v /home/zhouming/projects/universal-agent/:/hilp -v /home/zhouming/.d4rl:/root/.d4rl --name gcrl hilp /bin/bash -c "PYTHONPATH=. python example/hilp_gcrl.py --env_name antmaze-large-diverse-v2 --use_rnd --device cuda"
+# docker run -d --gpus device=0 -v /home/zhouming/projects/universal-agent/:/hilp -v /home/zhouming/.d4rl:/root/.d4rl --name gcrl hilp /bin/bash -c "PYTHONPATH=. python examples/hilp_gcrl.py --env-name antmaze-large-diverse-v2 --use-rnd 1 --device cuda"
