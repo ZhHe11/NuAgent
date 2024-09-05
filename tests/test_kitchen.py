4c3ae9e9d39a9b0742e0f21c44fe0a934998d920
ZhHe11 <1125447881@qq.com>
2024-09-05 07:48:31 +0000

    before rosurl

--- tests/test_kitchen.py
+++ tests/test_kitchen.py
@@ -1,6 +1,6 @@
 import sys
 import os
-# os.environ["MUJOCO_GL"] = "osmesa"
+os.environ["MUJOCO_GL"] = "osmesa"
 from garagei.envs.consistent_normalized_env import consistent_normalize
 from iod.utils import get_normalizer_preset
 
@@ -28,65 +28,108 @@ def vec_norm(vec):
     return vec / (torch.norm(vec, p=2, dim=-1, keepdim=True) + 1e-8)
     
 # 加载模型
-load_option_policy_base = torch.load("/data/zh/project12_Metra/METRA/exp/SGN/SGN_D_neg-len_encouragesd000_1724994434_kitchen_metra/wandb/latest-run/filesoption_policy.pt")
-load_traj_encoder_base = torch.load("//data/zh/project12_Metra/METRA/exp/SGN/SGN_D_neg-len_encouragesd000_1724994434_kitchen_metra/wandb/latest-run/filestaregt_traj_encoder.pt")
+# baseline
+load_option_policy_base = torch.load("/data/zh/project12_Metra/METRA/exp/Debug/sd000_1725467783_kitchen_metra/wandb/run-20240904_163624-487z1jec/filesoption_policy.pt")
+load_traj_encoder_base = torch.load("/data/zh/project12_Metra/METRA/exp/Debug/sd000_1725467783_kitchen_metra/wandb/run-20240904_163624-487z1jec/filestaregt_traj_encoder.pt")
+
+# # ours
+# load_option_policy_base = torch.load("/data/zh/project12_Metra/METRA/exp/kitchen/Target_traj_phi_learning-discrete-sd000_1725467722_kitchen_causer/wandb/latest-run/filesoption_policy.pt")
+# load_traj_encoder_base = torch.load("/data/zh/project12_Metra/METRA/exp/kitchen/Target_traj_phi_learning-discrete-sd000_1725467722_kitchen_causer/wandb/latest-run/filestaregt_traj_encoder.pt")
+
 policy = load_option_policy_base['policy']
 traj_encoder = load_traj_encoder_base['target_traj_encoder']
 
 # settings：
 max_path_length = 50
+option_dim = load_option_policy_base['dim_option']
+path = '/data/zh/project12_Metra/METRA/tests/videos/local_test/'
+Given_g = False
+PhiPlot = True
+num_task = 6
 
 # 初始化
 obs = env.reset()
-frames = []
 obs_tensor = torch.tensor(obs, dtype=torch.float).unsqueeze(0).to('cuda')
+Trajs = []
 device = 'cuda'
 
-
 # 加载goal
+support_options = torch.eye(option_dim).to(device)
 metric_success_task_relevant = {}
 metric_success_all_objects = {}
-all_goal_obs = []
-for i in range(6):
-    goal_obs = env.render_goal(i)
-    all_goal_obs.append(goal_obs)
-    metric_success_task_relevant[i] = 0
-    metric_success_all_objects[i] = 0
-all_goal_obs_tensor = torch.tensor(all_goal_obs, dtype=torch.float)
+if Given_g:
+    all_goal_obs = []
+    for i in range(num_task):
+        goal_obs = env.render_goal(i)
+        all_goal_obs.append(goal_obs)
+        metric_success_task_relevant[i] = 0
+        metric_success_all_objects[i] = 0
+    all_goal_obs_tensor = torch.tensor(all_goal_obs, dtype=torch.float)
+    eval_times = num_task
+else:
+    if option_dim == 2:
+        directions = [[1,0],[1,1],[0,1],[-1,1],[-1,0],[-1,-1],[0,-1],[1,-1]]
+        support_options = torch.tensor(directions).to(device)
+    eval_times = support_options.shape[0]
 
-for i in range(all_goal_obs_tensor.shape[0]):
-    goal_tensor = torch.tile(all_goal_obs_tensor[i].reshape(-1), (3,1)).reshape(-1).unsqueeze(0).to('cuda')
-    
-    for t in trange(max_path_length):
-        # policy
-        phi_s = traj_encoder(obs_tensor).mean
+# interact with env
+for i in trange(eval_times):
+    # 初始化
+    obs = env.reset()
+    frames = []
+    obs_tensor = torch.tensor(obs, dtype=torch.float).unsqueeze(0).to('cuda')
+    if Given_g:
+        goal_tensor = torch.tile(all_goal_obs_tensor[i].reshape(-1), (3,1)).reshape(-1).unsqueeze(0).to('cuda')
         phi_g = traj_encoder(goal_tensor).mean
-        option = vec_norm(phi_g - phi_s)
-        print('option', option)
+    else:
+        support_option = support_options[i].unsqueeze(0)
+    if PhiPlot: 
+        Traj = []
+        if Given_g:
+            Traj.append(phi_g)
+    # 每一条轨迹
+    for t in trange(max_path_length):
+        # policy inference:
+        phi_s = traj_encoder(obs_tensor).mean
+        if Given_g: 
+            # to do; 需要映射；
+            option = vec_norm(phi_g - phi_s)
+        else:
+            option = support_option
         obs_option = torch.cat((obs_tensor, option), -1).float()
         action_tensor = policy(obs_option)[1]['mean']
         action = action_tensor[0].detach().cpu().numpy()
-        
-        # iteration
+        # iteration:
         obs, reward, _, info = env.step(action)
         obs_tensor = torch.tensor(obs, dtype=torch.float).unsqueeze(0).to('cuda')
-        
-        # for viz
+        # for viz:
         obs_img = info['image']
         frames.append(obs_img)
-        # for metrics
-        k = 'metric_success_task_relevant/goal_'+str(i)
-        metric_success_all_objects[i] = max(metric_success_all_objects[i], info[k])
-        k = 'metric_success_all_objects/goal_'+str(i)   
-        metric_success_all_objects[i] = max(metric_success_all_objects[i], info[k])
-        
-        print('success', env.compute_success(i))
-    
-    gif_name = '/data/zh/project12_Metra/METRA/tests/videos/local_test/' + str(i) + '.gif'
+        if PhiPlot:
+            Traj.append(phi_s)
+            
+    # metrics:
+    success = np.zeros(num_task)
+    for id_task in range(num_task):
+        success[id_task] = env.compute_success(id_task)[0]
+    print('success', success, 'option', option)
+    # save traj. as gif
+    gif_name = path+ str(i) + '.gif'
     imageio.mimsave(gif_name, frames, 'GIF', duration=1)
     print('saved', gif_name)
+    Trajs.append(Traj)
     
-print('metric_success_task_relevant:', sum(metric_success_task_relevant.values()) / len(metric_success_task_relevant))
-print('metric_success_all_objects:',  sum(metric_success_all_objects.values()) / len(metric_success_all_objects))
+# PCA to 2 dim for viz
+Trajs_array = torch.concatenate([torch.concatenate(i) for i in Trajs]).detach().to('cpu').numpy()    # [batch*time, dim]
+with open (path + 'Trajs.npy', 'wb') as f:
+    np.save(f, Trajs_array)
+Trajs_array_2dim = PCA(n_components=2).fit_transform(Trajs_array)    # [batch*time, 2]
+Trajs_array_2dim = Trajs_array_2dim.reshape(len(Trajs), max_path_length, 2)    # [batch, time, 2]
+colors = cm.rainbow(np.linspace(0, 1, len(Trajs)))
+plt.figure()
+for i in range(len(Trajs)):
+    plt.plot(Trajs_array_2dim[i,:,0], Trajs_array_2dim[i,:,1], color=colors[i], label=str(i))
+plt.legend()
+plt.savefig(path + 'phi_traj.png')
 
 
