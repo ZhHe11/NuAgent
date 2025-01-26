ff036593a9c4f996aead0987a2d15f802bf44065
zhhe11 <1125447881@qq.com>
2025-01-26 20:11:15 +0800

    save

--- iod/viz_utils.py
+++ iod/viz_utils.py
@@ -289,19 +289,6 @@ def eval_cover_rate(env, agent_traj_encoder, agent_policy, dim_option, device, a
         arrive = 0
         for t in range(max_path_length):
             phi_obs_ = agent_traj_encoder(obs).mean
-            # # if baseline change everytime
-            # if option_type == 'baseline':
-            #     if t % 120 == 0:
-            #         obs_tmp = copy.deepcopy(obs)
-            #         target_obs = env.get_target_obs(obs_tmp, tensor_goal)
-            #         phi_target_obs = agent_traj_encoder(target_obs).mean
-            #     option = _vec_norm(phi_target_obs - phi_obs_)
-            # else:
-            #     if t % 120 == 0:
-            #         obs_tmp = copy.deepcopy(obs)
-            #         target_obs = env.get_target_obs(obs_tmp, tensor_goal)
-            #         phi_target_obs = agent_traj_encoder(target_obs).mean
-                
             
             obs_option = torch.cat((obs, option), -1).float()
             # for viz
