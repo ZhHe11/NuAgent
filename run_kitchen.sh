77aefe55b4a3b18885b98bc8fb1ebcb7fa4347a1
zhanghe3090 <1125447881@qq.com>
2025-03-27 03:51:58 +0000

    metra_ours

--- run_kitchen.sh
+++ run_kitchen.sh
@@ -11,9 +11,12 @@ export MUJOCO_GL="osmesa"
 
 # Policy
 python tests/main.py --run_group kitchen --env kitchen --max_path_length 50 --seed 2 --traj_batch_size 8 --n_parallel 4 --normalizer_type off --sac_max_buffer_size 100000  --n_epochs_per_log 25 --n_epochs_per_eval 25 --n_epochs_per_save 1000 --n_epochs_per_pt_save 1000 --discrete 1 --dim_option 24 --encoder 1 --sample_cpu 0 --sac_lr_a -1  --frame_stack 3  \
+    --algo metra_bl_ours --exp_name ours1-w2_05-win_5_2 --phi_type baseline --explore_type SZN --policy_type baseline --sample_type baseline --num_her 0 --trans_optimization_epochs 100 --target_theta 1 --is_wandb 1 --trans_minibatch_size 256 --common_lr 1e-4 --lr_te 1e-4 --dual_lr 1e-4 --SZN_w2 0.5 --SZN_w3 1 --SZN_window_size 5 --SZN_repeat_time 2 --Repr_temperature 0 --Repr_max_step 300 --z_unit 0 --model_master_num_layers 2 --n_epochs 2000 --wandb_note '' --save_pt_step 100  --seed 0  --SZN_std_min 1e-1
+
+
     --algo metra_bl --exp_name metra_bl --sac_lr_a -1 --trans_optimization_epochs 100 --n_epochs_per_log 25 --n_epochs_per_eval 25 --n_epochs_per_save 1000 --n_epochs_per_pt_save 1000 --discrete 1 --dim_option 24 --encoder 1 --sample_cpu 0 --is_wandb 1
 
-    --algo metra_bl_ours --exp_name ours1-win_5_2 --phi_type baseline --explore_type SZN --policy_type baseline --sample_type baseline --num_her 0 --trans_optimization_epochs 100 --target_theta 1 --is_wandb 1 --trans_minibatch_size 256 --common_lr 1e-4 --lr_te 1e-4 --dual_lr 1e-4 --SZN_w2 0 --SZN_w3 1 --SZN_window_size 5 --SZN_repeat_time 2 --Repr_temperature 0 --Repr_max_step 300 --z_unit 0 --model_master_num_layers 2 --n_epochs 2000 --wandb_note '' --save_pt_step 100  --seed 0  --SZN_std_min 1e-1
+
 
 
     --algo SZPC --exp_name kitchen-w2_5-window_5_2 --phi_type baseline --explore_type SZN --policy_type baseline --sample_type baseline --num_her 0 --trans_optimization_epochs 100 --target_theta 1 --is_wandb 1 --trans_minibatch_size 256 --common_lr 1e-4 --lr_te 1e-4 --dual_lr 1e-4 --SZN_w2 5 --SZN_w3 1 --SZN_window_size 5 --SZN_repeat_time 2 --Repr_temperature 0 --Repr_max_step 300 --z_unit 0 --model_master_num_layers 2 --n_epochs 2000 --wandb_note '' --save_pt_step 100  --seed 0  --SZN_std_min 1e-1
